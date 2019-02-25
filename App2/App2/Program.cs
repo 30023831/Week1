8638e2e63644b64914008c532b789f2b6d60ce56
Phillip DeVantier <30023831@student.toiohomai.ac.nz>
2019-02-25 17:04:58 +1300

    add app2

--- App2/App2/Program.cs
+++ App2/App2/Program.cs
@@ -8,15 +8,29 @@ namespace App2
 {
     class Program
     {
-        static void Main(string[] args)
+        /*public static void Main(string[] args)
+      
         {
         }
-
+        */
         public static int Sum(int num1, int num2)
         {
             int total;
             total = num1 + num2;
-                return total;
+            return total;
+        }
+
+        private static void Main()
+
+        {
+            Console.Write("\n\nFunction to calculate the sum of two numbers :\n");
+            Console.Write("--------------------------------------------------\n");
+	        Console.Write("Enter a number: ");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter another number: ");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nThe sum of two numbers is : {0} \n", Sum(n1, n2));
+            Console.ReadKey();
         }
 
 
