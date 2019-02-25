7b4200c8663d754be3d3321eed64954f2601c044
Phillip DeVantier <30023831@student.toiohomai.ac.nz>
2019-02-25 16:42:57 +1300

    First Commit

---
+++ App2/App2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int Sum(int num1, int num2)
+        {
+            int total;
+            total = num1 + num2;
+                return total;
+        }
+
+
+    }
+}
