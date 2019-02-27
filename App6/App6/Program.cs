349fdd7ee2af043d8b508f10650f27fd97701624
Phillip DeVantier <30023831@student.toiohomai.ac.nz>
2019-02-27 13:05:10 +1300

    stage3

---
+++ App6/App6/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r, per_cir;
+            double PI = 3.14;
+            double pie = 3.141592654;
+            Console.WriteLine("Input the radius of the circle : ");
+            r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Perimeter of Circle with radius {1}: {0}", Perimeter(r, pie), r);
+            Console.WriteLine("Area: {0}" , FindArea(r));
+            Console.Read();
+        }
+
+        static double Perimeter(double r, double pi)
+        {
+            return 2 * pi * r;
+        }
+
+        static double FindArea(double radius)
+
+        {
+            double area;
+            area = Math.PI* radius * radius;
+
+            return area;
+        }
+
+
+    }
+}
---
+++ App8/App8/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] randomNum = new int[1000];
+            Random RandomNumber = new Random();
+
+
+            for (int i = 0; i < 1000; i++)
+            {
+                randomNum[i] = RandomNumber.Next(1, 1000);
+            }
+
+
+            foreach (int j in randomNum)
+            {
+                Console.WriteLine("First Number:{0}", j);
+                Console.Read();
+            }
+
+        }
+
+    }
+}
