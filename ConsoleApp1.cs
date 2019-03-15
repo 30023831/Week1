f7da051399018fce781325dde7fcf1d5c0fb8477
Phillip DeVantier <30023831@student.toiohomai.ac.nz>
2019-03-15 14:23:29 +1300

    update4

---
+++ ConsoleApp1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[5] { 1, 2, 3, 4, 5 };
+            int[] copy = new int[10];
+            nums.CopyTo(copy, 5);
+            foreach(int x in copy)
+                Console.WriteLine(x);
+
+            Array.Copy(nums, copy, 5);
+            foreach (int x in copy)
+                Console.WriteLine(x);
+        }
+    }
+}
