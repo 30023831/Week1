4aabf510e7d75981e7ebea1d212d81c1075f05d3
Phillip DeVantier <30023831@student.toiohomai.ac.nz>
2019-03-22 14:04:48 +1300

    updater

---
+++ api/Stacks/Stacks/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Stacks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack s1 = new Stack();
+            string tryAgain = "";
+            while (tryAgain != "n")
+            {
+                Console.Write("Would you like to add an item(y/n): ");
+                tryAgain = Console.ReadLine().ToLower();
+                if (tryAgain == "y")
+                {
+                    Console.Write("Please enter something: ");
+                    s1.Push(Console.ReadLine());
+                }
+                else
+                    break;
+            }
+            /*Using the Count property to veiw the number of elements in the stack*/
+            Console.WriteLine("\n... Count ...");
+            Console.WriteLine("Count: " + s1.Count);
+            Console.Write("What would you like to look for: ");
+            string usercheck = Console.ReadLine();
+           bool check = false;
+
+            if (s1.Contains(usercheck))
+                check = true;
+
+            while (check)
+            {
+                if (s1.Peek().ToString() != usercheck)
+                {
+                    Console.WriteLine(s1.Pop()+" Removed");
+                }
+                else if(s1.Peek().ToString() == usercheck)
+                {
+                    Console.WriteLine("Item found.");
+                    check = false;
+                }
+            }
+            Console.WriteLine("Program finnished...");
+            Console.ReadLine();
+        }
+
+        
+            
+    }
+}
--- api/api1/api1/Program.cs
+++ api/api1/api1/Program.cs
@@ -10,6 +10,55 @@ namespace api1
     {
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.White;
+            string json = new WebClient().DownloadString("https://uinames.com/api/?ext&amount=500");
+
+            //Console.WriteLine(json);
+            //Console.ReadLine();
+            //Console.Clear();
+
+            List<Person> lst = new JavaScriptSerializer().Deserialize<List<Person>>(json);
+
+            foreach (Person x in lst)
+            {
+                Console.WriteLine($"Name: {x.name}\nAge: {x.age}");
+            }
+            Console.ReadLine();
         }
+
     }
+
+
+    public class Person
+    {
+        public string name { get; set; }
+        public string surname { get; set; }
+        public string gender { get; set; }
+        public string region { get; set; }
+        public int age { get; set; }
+        public string title { get; set; }
+        public string phone { get; set; }
+        public Birthday birthday { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public CreditCard credit_card { get; set; }
+        public string photo { get; set; }
+
+    }
+    public class Birthday
+    {
+        public string dmy { get; set; }
+        public string mdy { get; set; }
+        public int raw { get; set; }
+
+    }
+    public class CreditCard
+
+    {
+        public string expiration { get; set; }
+        public string number { get; set; }
+        public int pin { get; set; }
+        public int security { get; set; }
+    }
+
 }
